b5cc057fc86296dec95a786aa70bb153e5cbca46
ebana27 <ebanaplamedy@gmail.com>
2025-11-17 14:56:30 +0100

    c

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        onboarding: './onboarding.html',
+        signup: './signup.html',
+        gallery: './gallery.html'
+      }
+    }
   }
 })
