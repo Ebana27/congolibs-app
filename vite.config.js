3b32197f79b726da3729f5b850766cce10e8c088
ebana27 <ebanaplamedy@gmail.com>
2025-11-17 18:16:39 +0100

    c

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
-        onboarding: './onboarding.html'
+        // onboarding: './onboarding.html'
         // signup: './signup.html',
         // gallery: './gallery.html'
       }
